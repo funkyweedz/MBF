63ff7a11d9e887243148ffd5654bb16c2b2bd572
smolcat <smolcat@smolcats-MacBook-Air.local>
2026-04-29 13:47:52 +0100

    fix: ultra-compact Hero with integrated social media, no scroll needed

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import Navigation from './components/Navigation';
 import Hero from './components/Hero';
 import ProjectGallery from './components/ProjectGallery';
@@ -7,23 +7,32 @@ import ProjectDetail from './components/ProjectDetail';
 import ZelligePattern from './components/ZelligePattern';
 import Footer from './components/Footer';
 
+function AppContent() {
+  const location = useLocation();
+  const isHome = location.pathname === '/';
+
+  return (
+    <div className="relative min-h-screen flex flex-col">
+      <div className="fixed inset-0 opacity-5 pointer-events-none">
+        <ZelligePattern className="w-full h-full" />
+      </div>
+      <Navigation />
+      <div className="flex-1">
+        <Routes>
+          <Route path="/" element={<Hero />} />
+          <Route path="/projects" element={<ProjectGallery />} />
+          <Route path="/project/:id" element={<ProjectDetail />} />
+        </Routes>
+      </div>
+      {!isHome && <Footer />}
+    </div>
+  );
+}
+
 function App() {
   return (
     <Router>
-      <div className="relative min-h-screen flex flex-col">
-        <div className="fixed inset-0 opacity-5 pointer-events-none">
-          <ZelligePattern className="w-full h-full" />
-        </div>
-        <Navigation />
-        <div className="flex-1">
-          <Routes>
-            <Route path="/" element={<Hero />} />
-            <Route path="/projects" element={<ProjectGallery />} />
-            <Route path="/project/:id" element={<ProjectDetail />} />
-          </Routes>
-        </div>
-        <Footer />
-      </div>
+      <AppContent />
     </Router>
   );
 }
